e8018eda83a52c40bdfe11dcd37ee8be46418944
bstee615 <bstee615@students.bju.edu>
2018-10-12 20:30:45 -0400

    Renamed some unit tests.

--- instructiontests/tst_instructiontests.cpp
+++ instructiontests/tst_instructiontests.cpp
@@ -21,27 +21,27 @@ public:
 private slots:
     void init();
     void cleanup();
-    void mov_Success();
-    void movWithBigConstant_Success();
-    void movWithImmediateShiftedRegister_Success();
-    void movWithRegisterShiftedRegister_Success();
-    void mvn_Success();
-    void add_Success();
-    void addToSelf_Success();
-    void sub_Success();
-    void rsb_Success();
-    void and_Success();
-    void orr_Success();
-    void eor_Success();
-    void bic_Success();
-    void mul_Success();
+    void mov();
+    void movWithBigConstant();
+    void movWithImmediateShiftedRegister();
+    void movWithRegisterShiftedRegister();
+    void mvn();
+    void add();
+    void addToSelf();
+    void sub();
+    void rsb();
+    void _and();
+    void orr();
+    void eor();
+    void bic();
+    void mul();
 
-    void ldr_Success();
-    void str_Success();
-    void ldrWithImmediateOffset_Success();
-    void strWithImmediateOffset_Success();
-    void ldrWithRegisterOffset_Success();
-    void strWithRegisterOffset_Success();
+    void ldr();
+    void str();
+    void ldrWithImmediateOffset();
+    void strWithImmediateOffset();
+    void ldrWithRegisterOffset();
+    void strWithRegisterOffset();
 
     void pushpopWithoutWriteback();
     void pushpopWithWriteback();
@@ -78,99 +78,99 @@ void InstructionTests::cleanup()
     cpu->getRAM()->clearMemory();
 }
 
-void InstructionTests::mov_Success()
+void InstructionTests::mov()
 {
     assert_Instruction(0xe3a02030, QString("mov r2, #48"), 2, 48);
 }
 
-void InstructionTests::movWithBigConstant_Success()
+void InstructionTests::movWithBigConstant()
 {
     assert_Instruction(0xe3a00fb5, QString("mov r0, #724"), 0, 724);
 }
 
-void InstructionTests::movWithImmediateShiftedRegister_Success()
+void InstructionTests::movWithImmediateShiftedRegister()
 {
     cpu->setGeneralRegister(1, 0xa1000000);
     assert_Instruction(0xe1a02141, QString("mov r2, r1, asr #2"), 2, 0xe8400000);
 }
 
-void InstructionTests::movWithRegisterShiftedRegister_Success()
+void InstructionTests::movWithRegisterShiftedRegister()
 {
     cpu->setGeneralRegister(8, 0xa1000000);
     cpu->setGeneralRegister(10, 0x00000005);
     assert_Instruction(0xe1a06a38, QString("mov r6, r8, lsr r10"), 6, 0x05080000);
 }
 
-void InstructionTests::mvn_Success()
+void InstructionTests::mvn()
 {
     assert_Instruction(0xe3e07fb5, QString("mvn r7, #724"), 7, 0xfffffd2b);
 }
 
-void InstructionTests::add_Success()
+void InstructionTests::add()
 {
     cpu->setGeneralRegister(5, 0x000dabba);
     assert_Instruction(0xe2852019, QString("add r2, r5, #25"), 2, 0x000dabba + 25);
 }
 
-void InstructionTests::addToSelf_Success()
+void InstructionTests::addToSelf()
 {
     cpu->setGeneralRegister(5, 0x0baddeed);
     assert_Instruction(0xe285500F, QString("add r5, r5, #15"), 5, 0x0baddeed + 15);
 }
 
-void InstructionTests::sub_Success()
+void InstructionTests::sub()
 {
     cpu->setGeneralRegister(7, 0xfaded000);
     cpu->setGeneralRegister(6, 0x00bedfab);
     assert_Instruction(0xe0465007, QString("sub r5, r6, r7"), 5, 0x00bedfab - 0xfaded000);
 }
 
-void InstructionTests::rsb_Success()
+void InstructionTests::rsb()
 {
     cpu->setGeneralRegister(1, 0xdeadbeef);
     cpu->setGeneralRegister(4, 0xbadd000d);
     assert_Instruction(0xe0612604, QString("rsb r2, r1, r4, lsl #12"), 2, 0xd000d000 - 0xdeadbeef);
 }
 
-void InstructionTests::and_Success()
+void InstructionTests::_and()
 {
     cpu->setGeneralRegister(0, 0xcafebabe);
     assert_Instruction(0xe200101d, QString("and r1, r0, #29"), 1, 28);
 }
 
-void InstructionTests::orr_Success()
+void InstructionTests::orr()
 {
     cpu->setGeneralRegister(6, 0xfeedbeef);
     assert_Instruction(0xe3861a04, QString("orr r1, r6, #16384"), 1, 0xfeedfeef);
 }
 
-void InstructionTests::eor_Success()
+void InstructionTests::eor()
 {
     cpu->setGeneralRegister(3, 0x20);
     assert_Instruction(0xe22340a0, QString("eor r4, r3, #160"), 4, 0x00000080);
 }
 
-void InstructionTests::bic_Success()
+void InstructionTests::bic()
 {
     cpu->setGeneralRegister(0, 0xcafebabe);
     assert_Instruction(0xe3c01015, QString("bic r1, r0, #21"), 1, 0xcafebaaa);
 }
 
-void InstructionTests::mul_Success()
+void InstructionTests::mul()
 {
     cpu->setGeneralRegister(1, 0xa1000000);
     cpu->setGeneralRegister(2, 0x00001050);
     assert_Instruction(0xe0050291, QString("mul r5, r1, r2"), 5, 0x50000000);
 }
 
-void InstructionTests::ldr_Success()
+void InstructionTests::ldr()
 {
     cpu->setGeneralRegister(2, 0x100);
     cpu->getRAM()->WriteWord(0x100, 0x12345678);
     assert_Instruction(0xe5925000, QString("ldr r5, [r2]"), 5, 0x12345678);
 }
 
-void InstructionTests::str_Success()
+void InstructionTests::str()
 {
     cpu->setGeneralRegister(1, 0xd000000d);
     cpu->setGeneralRegister(2, 0x100);
@@ -178,14 +178,14 @@ void InstructionTests::str_Success()
     Q_ASSERT(cpu->getRAM()->ReadWord(0x100) == 0xd000000d);
 }
 
-void InstructionTests::ldrWithImmediateOffset_Success()
+void InstructionTests::ldrWithImmediateOffset()
 {
     cpu->setGeneralRegister(2, 0x104);
     cpu->getRAM()->WriteWord(0x100, 0x10203040);
     assert_Instruction(0xe5126004, QString("ldr r6, [r2, -#4]"), 6, 0x10203040);
 }
 
-void InstructionTests::strWithImmediateOffset_Success()
+void InstructionTests::strWithImmediateOffset()
 {
     cpu->setGeneralRegister(1, 0xd0d0dead);
     cpu->setGeneralRegister(5, 0x10c);
@@ -193,7 +193,7 @@ void InstructionTests::strWithImmediateOffset_Success()
     Q_ASSERT(cpu->getRAM()->ReadWord(0x100) == 0xd0d0dead);
 }
 
-void InstructionTests::ldrWithRegisterOffset_Success()
+void InstructionTests::ldrWithRegisterOffset()
 {
     cpu->setGeneralRegister(2, 0x050);
     cpu->setGeneralRegister(4, 0x0b0);
@@ -202,7 +202,7 @@ void InstructionTests::ldrWithRegisterOffset_Success()
     assert_Instruction(0xe7b28004, QString("ldr r8, [r2, r4]!"), 2, 0x100);
 }
 
-void InstructionTests::strWithRegisterOffset_Success()
+void InstructionTests::strWithRegisterOffset()
 {
     cpu->setGeneralRegister(8, 0xfad15ded);
     cpu->setGeneralRegister(5, 0x4);
@@ -210,7 +210,6 @@ void InstructionTests::strWithRegisterOffset_Success()
 
     // Without writeback
     assert_Instruction(0xe7858007, QString("str r8, [r5, r7]"), 5, 0x4);
-    qDebug() << cpu->getRAM()->ReadWord(0xc);
     Q_ASSERT(cpu->getRAM()->ReadWord(0xc) == 0xfad15ded);
 
     // With writeback
